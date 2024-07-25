eb1e6035cf14a4145033dd30e06a66e9cb3f0ddc
William <william.deazevedopro@gmail.com>
2024-07-25 17:13:07 +0200

    Responsive fix and slide

--- components/IconsSlider.jsx
+++ components/IconsSlider.jsx
@@ -1,160 +1,54 @@
 "use client";
 import Image from "next/image";
 
+const iconData = [
+  { src: "/React-icon.png", alt: "React Logo icon" },
+  { src: "/zustand-logo.png", alt: "Zustand Logo icon", width: 70, height: 70 }, // Ajusté
+  { src: "/next.svg", alt: "Next JS Logo icon", width: 80 },
+  { src: "/Tailwind_CSS_Logo.png", alt: "Tailwind CSS Logo icon" },
+  { src: "/Typescript_logo.png", alt: "TypeScript Logo Icon" },
+  { src: "/Figma_logo.png", alt: "Figma Logo Icon" },
+  {
+    src: "/react-router-logo.png",
+    alt: "React Router Logo icon",
+    width: 80,
+    height: 50,
+  },
+  { src: "/react-query-logo.png", alt: "React Query Logo icon", width: 80 },
+  { src: "/Redux_logo.png", alt: "Redux toolkit Logo icon" },
+  { src: "/Javascript-logo.png", alt: "Javascript Logo Icon" },
+];
+
+const SliderItem = ({ src, alt, width = 70, height = 70 }) => (
+  <div className="slider-item inline-block mx-4">
+    <Image
+      className="w-14 lg:w-[70px] object-contain"
+      src={src}
+      width={width}
+      height={height}
+      alt={alt}
+    />
+  </div>
+);
+
 const IconsSlider = () => {
+  const firstSliderIcons = iconData.slice(0, 5);
+  const secondSliderIcons = iconData.slice(5);
+
   return (
     <>
-      <div className="slider">
-        <div className="slide-track">
-          <div className="slider-item">
-            <Image
-              src="/React-icon.png"
-              width={70}
-              height={70}
-              alt="React Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/next.svg"
-              width={80}
-              height={70}
-              alt="Next JS Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Tailwind_CSS_Logo.png"
-              width={70}
-              height={70}
-              alt="Tailwind CSS Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Typescript_logo.png"
-              width={70}
-              height={70}
-              alt="TypeScript Logo Icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/React-icon.png"
-              width={70}
-              height={70}
-              alt="React icon"
-            />
-          </div>
-
-          <div className="slider-item">
-            <Image
-              src="/Figma_logo.png"
-              width={70}
-              height={70}
-              alt="Figma Logo Icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/next.svg"
-              width={80}
-              height={70}
-              alt="Next JS Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Tailwind_CSS_Logo.png"
-              width={70}
-              height={70}
-              alt="Tailwind CSS Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Typescript_logo.png"
-              width={70}
-              height={70}
-              alt="TypeScript Logo Icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Figma_logo.png"
-              width={70}
-              height={70}
-              alt="Figma Logo Icon"
-            />
-          </div>
+      <div className="slider overflow-hidden whitespace-nowrap my-8">
+        <div className="slider-track inline-block animate-slide">
+          {[...firstSliderIcons, ...firstSliderIcons].map((icon, index) => (
+            <SliderItem key={`first-${index}`} {...icon} />
+          ))}
         </div>
       </div>
-      <div className="slider">
-        <div className="slider-track-2">
-          <div className="slider-item">
-            <Image
-              src="/react-router-logo.png"
-              width={80}
-              height={50}
-              alt="React Router Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/react-query-logo.png"
-              width={80}
-              height={70}
-              alt="Ruby Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Redux_logo.png"
-              width={70}
-              height={70}
-              alt="Redux toolkit Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Javascript-logo.png"
-              width={70}
-              height={70}
-              alt="Javascript Logo Icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/react-router-logo.png"
-              width={80}
-              height={50}
-              alt="React Router Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/react-query-logo.png"
-              width={80}
-              height={70}
-              alt="Ruby Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Redux_logo.png"
-              width={70}
-              height={70}
-              alt="Redux toolkit Logo icon"
-            />
-          </div>
-          <div className="slider-item">
-            <Image
-              src="/Javascript-logo.png"
-              width={70}
-              height={70}
-              alt="Javascript Logo Icon"
-            />
-          </div>
+      <div className="slider overflow-hidden whitespace-nowrap my-8">
+        <div className="slider-track-2 inline-block animate-slide-reverse">
+          {[...secondSliderIcons, ...secondSliderIcons].map((icon, index) => (
+            <SliderItem key={`second-${index}`} {...icon} />
+          ))}
         </div>
       </div>
     </>
